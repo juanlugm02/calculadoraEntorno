7a8abaf3512d2d97e42e0d681741a60e820894fa
JUANLU <JUANLU@correo.com>
2021-05-06 13:25:07 +0200

    Añadiendo multiplicacion

--- src/gitentorno/principal.java
+++ src/gitentorno/principal.java
@@ -27,19 +27,19 @@ public class principal {
         int operacion =0;
         do {
             System.out.println(" Introduce Operacion a realizar");
-            System.out.println(" 1. suma \n 2. resta \n 3. salir");
+            System.out.println(" 1. suma \n 2. resta \n3. Multiplicacion \n 4. salir");
             // leo por teclado un entero como opción del menú
             operacion =sc.nextInt();
             // segun valor elegido ejecuto método del objeto claculadora
             switch ( operacion ){
             case 1: System.out.println( " la suma es "+ casio.suma() ); break;
             case 2: System.out.println( " la resta es "+ casio.resta() ); break;
-            //case 3: System.out.println(" la multipilcacion es "+ casio.multiplicacion()); break;
+            case 3: System.out.println(" la multipilcacion es "+ casio.multiplicacion()); break;
             //case 4: System.out.println(" La division es " + casio.division()); break;
-            default: System.out.println(" Debes elegir una opción de 1 a 3");
+            default: System.out.println(" Debes elegir una opción de 1 a 4");
             }
     
-        } while ( operacion != 3 );
+        } while ( operacion != 4 );
         
         System.out.println(" Gracias por usar la calculadora...hasta luegooooorrr");
         
